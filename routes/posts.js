f4599ec6d3588e7660892bbbf2246f09c5573ade
ahodot01 <s.selection@gmail.com>
2024-12-27 07:50:11 +0000

    posts.js update

--- routes/posts.js
+++ routes/posts.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const postController = require('../controllers/postController');
-const { verifyToken } = require('./routes/auth');
+const { verifyToken } = require('./auth');
 const router = express.Router();
 
 // VARIOUS ROUTES FOR ACTIVITES TO BE PERFORMED BY USERS
-router.post('/', authMiddleware, postController.createPost); // PERFORM POST
-router.get('/:topic', authMiddleware, postController.getPostsByTopic); // FETCH POSTS BY TOPIC
-router.post('/:postId/report', authMiddleware, postController.reportPost); // PERFORM REPORT
-router.post('/:postId/views', authMiddleware, postController.incrementPostViews); // POSTS' VIEW COUNT
-router.post('/:postId/like', authMiddleware, postController.likePost); // PERFORM LIKE
-router.post('/:postId/dislike', authMiddleware, postController.dislikePost); // PERFORM DISLIKE
-router.post('/:postId/comment', authMiddleware, postController.addComment); // PERFORM COMMENT
-router.get('/:topic/expired', authMiddleware, postController.getExpiredPosts); // GET EXPIRED POST/S
-router.get('/:topic/most-active', authMiddleware, postController.getMostActivePost); // MOST ACTIVE POST
+router.post('/', verifyToken, postController.createPost); // PERFORM POST
+router.get('/:topic', verifyToken, postController.getPostsByTopic); // FETCH POSTS BY TOPIC
+router.post('/:postId/report', verifyToken, postController.reportPost); // PERFORM REPORT
+router.post('/:postId/views', verifyToken, postController.incrementPostViews); // POSTS' VIEW COUNT
+router.post('/:postId/like', verifyToken, postController.likePost); // PERFORM LIKE
+router.post('/:postId/dislike', verifyToken, postController.dislikePost); // PERFORM DISLIKE
+router.post('/:postId/comment', verifyToken, postController.addComment); // PERFORM COMMENT
+router.get('/:topic/expired', verifyToken, postController.getExpiredPosts); // GET EXPIRED POST/S
+router.get('/:topic/most-active', verifyToken, postController.getMostActivePost); // MOST ACTIVE POST
 
 module.exports = router;
