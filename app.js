1108d28cf28ff4da4a08222ac2558531e24f2798
ahodot01 <s.selection@gmail.com>
2024-12-27 08:06:18 +0000

    Edited app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 // CONNECT TO MONGODB
 mongoose.connect(process.env.DB_CONNECTOR)
-  .then(() => console.log('MongoDB connected'))
+  .then(() => console.log('Connected to MongoDB successfully'))
   .catch(err => console.error('MongoDB connection error:', err));
 
 // ROUTES
